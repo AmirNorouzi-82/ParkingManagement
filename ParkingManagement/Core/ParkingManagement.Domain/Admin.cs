39fcc6dc9ec67607d1b8dc1e69b2121b5045120e
AmirNorouzi-82 <147101430+AmirNorouzi-82@users.noreply.github.com>
2026-01-03 22:39:12 +0330

    Adding ParkingAssignmentRequest Domain Class For signing New Car

--- ParkingManagement/Core/ParkingManagement.Domain/Admin.cs
+++ ParkingManagement/Core/ParkingManagement.Domain/Admin.cs
@@ -9,11 +9,8 @@ namespace ParkingManagement.Core.ParkingManagement.Domain
         public string PasswordHash { get; set; }
         public string Email { get; set; }
         public string FullName { get; set; }
-
         public bool IsActive { get; set; } = true;
-
         public DateTime CreatedAt { get; set; } = DateTime.Now;
-
         public DateTime? LastLogin { get; set; }
     }
 }
